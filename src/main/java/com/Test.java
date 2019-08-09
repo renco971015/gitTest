a3f66ba083eecdfcacef6f6af0c8fbcaa9c3e9d5
xurunhao <xurunhao@rmitec.cn>
2019-08-09 15:19:12 +0800

    项目第一次提交

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,19 @@
+package com;
+
+/**
+ * 名称：<br>
+ * 功能：〈功能详细描述〉<br>
+ * 方法：〈方法简述 - 方法描述〉<br>
+ * 版本：1.0 <br>
+ * 作者：xurunhao <br>
+ * 日期：2019/8/9 15:18 <br>
+ * 说明：<br>
+ */
+public class Test {
+
+    public static void main(String[] args){
+
+        System.out.println("测试第一分支");
+
+    }
+}
