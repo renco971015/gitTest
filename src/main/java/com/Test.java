825ad0218982efbec8986fe53cfa00a24b06d650
xurunhao <xurunhao@rmitec.cn>
2019-08-09 15:03:41 +0800

    第一次提交

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,20 @@
+package com;
+
+/**
+ * 名称：<br>
+ * 功能：〈功能详细描述〉<br>
+ * 方法：〈方法简述 - 方法描述〉<br>
+ * 版本：1.0 <br>
+ * 作者：xurunhao <br>
+ * 日期：2019/8/9 14:24 <br>
+ * 说明：<br>
+ */
+public class Test {
+
+    public static void  main(String[] args){
+
+        String test1 = "新增功能";
+
+        System.out.println(test1);
+    }
+}
