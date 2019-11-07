c86ffd2d299d2be632530a5c4b3b5026b90e3298
xurunhao <xurunhao@rmitec.cn>
2019-11-07 14:26:27 +0800

    rebase开发1

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -55,6 +55,9 @@ public class Test {
         String rebase8;
         System.out.println("rebase测试");
 
+        //rebase开发
+        System.out.println("rebase开发1");
+
         //业务代码开发
         System.out.println("dev业务开发提交1");
         System.out.println("dev业务开发提交2");
